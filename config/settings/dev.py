9dbaa19139912c17656d95d1feb97fd31eee13b7
grace287 <chenlucia287@gmail.com>
2024-10-28 15:11:23 +0900

    중간점검

---
+++ config/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# PostgreSQL 데이터베이스 설정
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'finance_web_app_db',
+        'USER': 'finance_user',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
---
+++ config/urls/prod.py
@@ -0,0 +1,7 @@
+from .base import *
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
