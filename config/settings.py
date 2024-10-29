6efda3ff24f7828decd1c0aacfe357085f905873
kimroha <doboduu@gmail.com>
2024-10-30 04:18:46 +0900

    데이터베이스연결지

--- config/settings.py
+++ config/settings.py
@@ -80,28 +80,28 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': BASE_DIR / 'db.sqlite3',
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'finance_web_app_db',
-#         'USER': 'finance_user',
-#         'PASSWORD': '1111',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#         'OPTIONS': {
-#             'client_encoding': 'UTF8',
-#         },
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 # }
+
+DATABASES = {
+     'default': {
+         # 'ENGINE': 'django.db.backends.sqlite3',
+         # 'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'finance',
+         'USER': 'rohakim',
+         'PASSWORD': '',
+         'HOST': 'localhost',
+         'PORT': '5432',
+         'OPTIONS': {
+             'client_encoding': 'UTF8',
+         },
+     }
+ }
 
 
 # Password validation
